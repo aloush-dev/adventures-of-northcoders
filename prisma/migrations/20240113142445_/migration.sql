e89f55017ae590288d86138b6bab63a364b0aa36
Ali <itsaliabdallah@gmail.com>
2024-01-13 21:57:00 +0000

    update db to use mongo

--- prisma/migrations/20240113142445_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `puzzleName` to the `UserSolution` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "UserSolution" ADD COLUMN     "puzzleName" TEXT NOT NULL;
