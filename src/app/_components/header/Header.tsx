14173b3720ba32a79e8161725afa67a8802f4514
Ali <itsaliabdallah@gmail.com>
2024-01-15 22:06:33 +0000

    Add link to home page to header text

--- src/app/_components/header/Header.tsx
+++ src/app/_components/header/Header.tsx
@@ -2,15 +2,18 @@ import React from "react";
 import { getServerAuthSession } from "~/server/auth";
 import ProfileView from "../header/ProfileView";
 import { SignInButton } from "../header/SignInButton";
+import Link from "next/link";
 
 export async function Header() {
   const session = await getServerAuthSession();
 
   return (
     <header className="flex items-center justify-between bg-gray-700 p-4 text-white">
-      <h1 className="text-3xl font-extrabold tracking-tight ">
-        A<span className="text-[#eb1c24]">o</span>N
-      </h1>
+      <Link href="/">
+        <h1 className="text-3xl font-extrabold tracking-tight ">
+          A<span className="text-[#eb1c24]">o</span>N
+        </h1>
+      </Link>
       <div className="h-auto w-auto">
         {session ? <ProfileView session={session} /> : <SignInButton />}
       </div>
