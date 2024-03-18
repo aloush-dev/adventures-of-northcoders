3bed1bebcd1855b78318d76164aa166e889ba079
Ali <itsaliabdallah@gmail.com>
2024-03-18 21:25:31 +0000

    temp typing fix

--- types/types.ts
+++ types/types.ts
@@ -2,7 +2,7 @@ export type Puzzle = {
   UserSolution: {
     part_1_complete: boolean;
     part_2_complete: boolean;
-    timeOpened: Date;
+    timeOpened: Date | null;
   }[];
 } & {
   collection: string;
